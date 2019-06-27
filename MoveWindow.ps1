ed79bd1cfddfb19bff13fc7d5c11849c98ad2bc5
XB71 <32171659+XB71@users.noreply.github.com>
2019-06-27 13:52:24 -0700

    Window mover 1

---
+++ MoveWindow.ps1
@@ -0,0 +1,15 @@
+﻿
+$wshell = New-Object -ComObject wscript.shell;
+$app = Start-Process "notepad"
+Sleep 3
+$wshell.AppActivate($app)
+Sleep 1
+$wshell.SendKeys('%' + " ")
+Sleep .5
+$wshell.SendKeys('{DOWN}')
+Sleep .5
+$wshell.SendKeys('~')
+Sleep .5
+$wshell.SendKeys('{RIGHT}')
+
+$wshell.SendKeys('{RWin down}')
